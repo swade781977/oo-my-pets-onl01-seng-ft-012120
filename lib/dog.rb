fdeb1013d2ca310f544314d0be868297fb30c963
Chris Wade <swade781977@yahoo.com>
2020-02-07 04:06:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,15 @@
 class Dog
   @@all = []
-  def initialize(name)
+  def initialize(name, owner)
     @name = name
-    @@all << name
+    @owner = owner
+    @@all << self
     @mood = "nervous"
   end 
   attr_reader :name  
   attr_accessor :mood, :owner 
   
+  def self.all
+    @@all
+  end
 end
