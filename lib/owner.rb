d2b79eb98396bce5767d151dcf427ab573a9e961
Chris Wade <swade781977@yahoo.com>
2020-02-07 05:43:23 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -42,6 +42,21 @@ class Owner
     Cat.all.each{|cat| cat.mood = "happy" if cat.owner == self}
   end
   
+  def sell_pets
+    Cat.all.each do |cat|
+      if cat.owner == self
+        cat.mood = "nervous"
+        cat.owner = nil 
+      end 
+    end
+    Dog.all.each do |dog|
+      if dog.owner == self
+        dog.mood = "nervous"
+        dog.owner = nil 
+      end
+    end
+  end
+  
   def self.all
     @@all
   end
